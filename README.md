5cb932a95dd2402ee5009ad7a70c8d9f3c3acb2a
Rico <d-Rickyy-b@users.noreply.github.com>
2021-04-12 01:32:50 +0200

    feat: initial commit - v0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-node-red-contrib-ffpass
+# node-red-contrib-ffpass
+
+This module enables you to control your Ford car via the FordPass API within [Node-RED](https://nodered.org/).
+
+### ⚠ Important note
+ffpass needs at least Node 12.x because it uses the [fromEntries()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries) method. Hence this module currently doesn't work with any earlier Node version.
---
+++ ffpass/ffpass.js
@@ -0,0 +1,244 @@
+module.exports = function (RED) {
+    "use strict";
+
+    const fordApi = require('ffpass')
+
+    function ffpassConfigNode(config) {
+        RED.nodes.createNode(this, config);
+        this.ford_username = config.ford_username;
+        this.ford_password = config.ford_password;
+        this.vin = config.vin;
+    }
+    RED.nodes.registerType("ffpass_config", ffpassConfigNode);
+
+    function ffpassStatus(config) {
+        RED.nodes.createNode(this, config);
+        this.config = RED.nodes.getNode(config.config);
+        const car = new fordApi.vehicle(this.config.ford_username, this.config.ford_password, this.config.vin)
+
+        var node = this;
+        node.on('input', async function (msg, send, done) {
+            try {
+                await car.auth()
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Auth failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "Auth successful" });
+
+            try {
+                var vehicleData = await car.status()
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Status failed" });
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "Status received" });
+
+            send(JSON.stringify(vehicleData));
+
+            /*
+            try {
+                car.auth().then(() => {
+                    node.warn("Auth successful")
+                    this.status({fill:"green",shape:"dot",text:"Auth successful"});
+                },
+                () => {
+                    node.warn("Auth failed")
+                    this.status({fill:"red",shape:"dot",text:"Auth failed"});
+                }).catch((error) => {
+                    node.warn(error);
+                    this.status({fill:"red",shape:"dot",text:"Auth failed"});
+                }).then(() => {
+                    car.status().then(vehicleData => {
+                        node.warn(vehicleData)
+                        this.status({fill:"green",shape:"dot",text:"status successful"});
+                        node.send(JSON.stringify(vehicleData));
+                    }, status => {
+                        node.warn(status)
+                        this.status({fill:"red",shape:"dot",text:"Fetching status failed: " + status});
+                        node.send(JSON.stringify(status));
+                    }).catch((error) => {
+                        node.warn(error);
+                        this.status({fill:"red",shape:"dot",text:"Fetching status failed"});
+                    })
+                });
+            } catch (error) {
+                node.warn(error);
+            }
+            
+            */
+        });
+    }
+
+    function ffpassIssueCommand(config) {
+        RED.nodes.createNode(this, config);
+        this.config = RED.nodes.getNode(config.config);
+        const car = new fordApi.vehicle(this.config.ford_username, this.config.ford_password, this.config.vin)
+
+        var node = this;
+        node.on('input', async function (msg) {
+            var command = msg.payload.toLowerCase()
+            if (["start", "stop", "lock", "unlock"].indexOf(command) >= 0) {
+                // Command is available
+                this.status({ fill: "green", shape: "dot", text: "'" + command + "' received." });
+
+                try {
+                    await car.auth()
+                } catch (err) {
+                    node.warn(err);
+                    this.status({ fill: "red", shape: "ring", text: "Auth failed" });
+                    // TODO: Add support for node < 1.0
+                    done(err)
+                }
+                this.status({ fill: "green", shape: "dot", text: "Auth successful" });
+
+                try {
+                    var result = await car.issueCommand(command)
+                } catch (err) {
+                    node.warn(err);
+                    this.status({ fill: "red", shape: "ring", text: "Command failed" });
+                    // TODO: Add support for node < 1.0
+                    done(err)
+                }
+                this.status({ fill: "green", shape: "dot", text: "'" + command + "' issued!" });
+                node.send(JSON.stringify(result));
+            } else {
+                node.warn(`Command '${command}' is not available! Please use any of ["start", "stop", "lock", "unlock"].`);
+            }
+        });
+    }
+
+    function ffpassLock(config) {
+        RED.nodes.createNode(this, config);
+        this.config = RED.nodes.getNode(config.config);
+        const car = new fordApi.vehicle(this.config.ford_username, this.config.ford_password, this.config.vin)
+
+        var node = this;
+        node.on('input', async function (msg) {
+            try {
+                await car.auth()
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Auth failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "Auth successful" });
+
+            try {
+                var result = await car.issueCommand("lock")
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Command failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "'lock' issued!" });
+
+            node.send(JSON.stringify(result));
+        });
+    }
+
+    function ffpassUnlock(config) {
+        RED.nodes.createNode(this, config);
+        this.config = RED.nodes.getNode(config.config);
+        const car = new fordApi.vehicle(this.config.ford_username, this.config.ford_password, this.config.vin)
+
+        var node = this;
+        node.on('input', async function (msg) {
+            try {
+                await car.auth()
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Auth failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "Auth successful" });
+
+            try {
+                var result = await car.issueCommand("unlock")
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Command failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "'unlock' issued!" });
+
+            node.send(JSON.stringify(result));
+        });
+    }
+
+    function ffpassStart(config) {
+        RED.nodes.createNode(this, config);
+        this.config = RED.nodes.getNode(config.config);
+        const car = new fordApi.vehicle(this.config.ford_username, this.config.ford_password, this.config.vin)
+
+        var node = this;
+        node.on('input', async function (msg) {
+            try {
+                await car.auth()
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Auth failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "Auth successful" });
+
+            try {
+                var result = await car.issueCommand("start")
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Command failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "'start' issued!" });
+
+            node.send(JSON.stringify(result));
+        });
+    }
+
+    function ffpassStop(config) {
+        RED.nodes.createNode(this, config);
+        this.config = RED.nodes.getNode(config.config);
+        const car = new fordApi.vehicle(this.config.ford_username, this.config.ford_password, this.config.vin)
+
+        var node = this;
+        node.on('input', async function (msg) {
+            try {
+                await car.auth()
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Auth failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "Auth successful" });
+
+            try {
+                var result = await car.issueCommand("stop")
+            } catch (err) {
+                node.warn(err);
+                this.status({ fill: "red", shape: "ring", text: "Command failed" });
+                // TODO: Add support for node < 1.0
+                done(err)
+            }
+            this.status({ fill: "green", shape: "dot", text: "'stop' issued!" });
+
+            node.send(JSON.stringify(result));
+        });
+    }
+
+    RED.nodes.registerType("ffpass-status", ffpassStatus);
+    RED.nodes.registerType("ffpass-command", ffpassIssueCommand);
+    RED.nodes.registerType("ffpass-lock", ffpassLock);
+    RED.nodes.registerType("ffpass-unlock", ffpassUnlock);
+    RED.nodes.registerType("ffpass-start", ffpassStart);
+    RED.nodes.registerType("ffpass-stop", ffpassStop);
+}
